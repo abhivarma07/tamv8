702aa15942a1ad602bb2f76552f452cb618128ae
abhivarma07 <abhijeetthemessi@gmail.com>
2022-03-10 11:15:34 +0530

    Added forms for quizzes

--- src/components/Slider/data.js
+++ src/components/Slider/data.js
@@ -1,15 +1,15 @@
 export const Quiz =[
     { id : 1 , name : "Sports And General 1st Years" , role : "Know More" , desc : "3/23/2022 WEDNESDAY",img : require("../../images/Quizes/q.png"),form : "https://ujv2s3mi.paperform.co",tar : "_blank"},
-    { id : 2 , name : "Entertainment 1st Years" , role : "Know More" , desc : "3/31/2022 THURSDAY",img : require("../../images/Quizes/entertainment.png") ,form : "https://rafmnric.paperform.co" , tar : "_blank"},
-    { id : 3 , name : "CodeVita" , role : "Know More" , desc : "4/6/2022 WEDNESDAY",img : require("../../images/Quizes/code.png"), form : "https://rafmnric.paperform.co", tar : "_blank"},
-    { id : 4 , name : "Circuitronics" , role : "Know More" , desc : "4/8/2022 FRIDAY",img : require("../../images/Quizes/circuit.png"), form : "https://rafmnric.paperform.co",tar : "_blank"},
-    { id : 5 , name : "Sports" , role : "Know More" , desc : "4/13/2022 WEDNESDAY",img : require("../../images/Quizes/sports.png"), form : "https://rafmnric.paperform.co",tar : "_blank"},
-    { id : 6 , name : "General" , role : "Know More" , desc : "4/18/2022 MONDAY",img : require("../../images/Quizes/gen.png"), form : "https://rafmnric.paperform.co",tar : "_blank"},
-    { id : 7 , name : "Entertainment" , role : "Know More" , desc : "4/20/2022 WEDNESDAY",img : require("../../images/Quizes/enter.png"), form : "https://rafmnric.paperform.co",tar : "_blank"},
+    { id : 2 , name : "Entertainment 1st Years" , role : "Know More" , desc : "3/31/2022 THURSDAY",img : require("../../images/Quizes/entertainment.png") ,form : "https://koqflvep.paperform.co" , tar : "_blank"},
+    { id : 3 , name : "CodeVita" , role : "Know More" , desc : "4/6/2022 WEDNESDAY",img : require("../../images/Quizes/code.png"), form : "https://4uaowdfg.paperform.co", tar : "_blank"},
+    { id : 4 , name : "Circuitronics" , role : "Know More" , desc : "4/8/2022 FRIDAY",img : require("../../images/Quizes/circuit.png"), form : "https://272wvgri.paperform.co",tar : "_blank"},
+    { id : 5 , name : "Sports" , role : "Know More" , desc : "4/13/2022 WEDNESDAY",img : require("../../images/Quizes/sports.png"), form : "https://3jccrmba.paperform.co",tar : "_blank"},
+    { id : 6 , name : "General" , role : "Know More" , desc : "4/18/2022 MONDAY",img : require("../../images/Quizes/gen.png"), form : "https://cda0kty1.paperform.co",tar : "_blank"},
+    { id : 7 , name : "Entertainment" , role : "Know More" , desc : "4/20/2022 WEDNESDAY",img : require("../../images/Quizes/enter.png"), form : "https://1sccitqb.paperform.co",tar : "_blank"},
 ] ;
 
 export const Events =[
-    { id : 1 , name : "Group Discussions" , role : "Know More" , desc : "3/24/2022 THURSDAY",img : require("../../images/Quizes/dis.png"), form : "https://rafmnric.paperform.co" , tar : "_blank"},
+    { id : 1 , name : "Group Discussions" , role : "Know More" , desc : "3/24/2022 THURSDAY",img : require("../../images/Quizes/dis.png"), form : "https://auwefngc.paperform.co" , tar : "_blank"},
     { id : 2 , name : "(HTML&CSS)- 1st years" , role : "Know More" ,desc : "3/25/2022 & 3/26/2022",img : require("../../images/tam3.png"), form : "https://vmzbfsfo.paperform.co",tar : "_blank"},
     { id : 3 , name : "(ARDUINO- 1ST YEARS)" , role : "Know More" ,desc : "4/4/2022 & 4/5/2022",img : require("../../images/tam4.png"),form : "https://tdqtazqo.paperform.co",tar : "_blank"},
     { id : 4 , name : "CYBER SECURITY" , role : "Know More" ,desc : "4/11/2022 & 4/12/2022",img : require("../../images/tam.png"),form : "https://fyx2p611.paperform.co",tar : "_blank"},
